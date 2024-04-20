30eafcc61162f302ffd11be412abb1b749119c39
Adriana Jacobo <adriana.jacobo@catolica.edu.sv>
2024-04-20 17:20:28 -0600

    Creacion de prototipos de formularios y configuracion de prototipo 1

---
+++ L02P02_2021JS650_2021SZ650/Controllers/AutoresController.cs
@@ -0,0 +1,27 @@
+﻿using L02P02_2021JS650_2021SZ650.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021JS650_2021SZ650.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private readonly LibreriaDbContext _libreriaDbContext;
+
+        public AutoresController(LibreriaDbContext libreriaDbContext)
+        {
+            _libreriaDbContext = libreriaDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var listadoDeAutores = (from e in _libreriaDbContext.Autores
+                                    select new
+                                    {
+                                        nombreautor = e.Autor,
+                                    }).ToList();
+            ViewData["listadoAutores"] = listadoDeAutores;
+
+            return View();
+        }
+    }
+}
---
+++ L02P02_2021JS650_2021SZ650/Controllers/ComentariosLibroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021JS650_2021SZ650.Controllers
+{
+    public class ComentariosLibroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
