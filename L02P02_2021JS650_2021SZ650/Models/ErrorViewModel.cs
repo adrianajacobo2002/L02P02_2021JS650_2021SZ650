0a9d75dc48f4b3dc8b6c1628a6d69bdfc6be1a3d
Adriana Jacobo <adriana.jacobo@catolica.edu.sv>
2024-04-20 14:20:11 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021JS650_2021SZ650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021JS650_2021SZ650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
