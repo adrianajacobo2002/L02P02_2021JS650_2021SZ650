a8d8497852d348e92ba14996d500e4d4107e5b04
Diego Saravia <saravia__2000@hotmail.com>
2024-04-20 18:43:04 -0600

    Libros obtenidos a partir de autor

--- L02P02_2021JS650_2021SZ650/Controllers/LibroController.cs
+++ L02P02_2021JS650_2021SZ650/Controllers/LibroController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using L02P02_2021JS650_2021SZ650.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace L02P02_2021JS650_2021SZ650.Controllers
 {
     public class LibroController : Controller
     {
-        public IActionResult Index()
+        private readonly LibreriaDbContext _libreriaDbContext;
+
+        public LibroController(LibreriaDbContext libreriaDbContext)
         {
+            _libreriaDbContext = libreriaDbContext;
+        }
+
+        public IActionResult Index(int id)
+        {
+            string nombreAutor = _libreriaDbContext.Autores.FirstOrDefault(x => x.Id == id)?.Autor ?? "";
+            var libros = (from l in _libreriaDbContext.Libros
+                          join a in _libreriaDbContext.Autores on l.IdAutor equals a.Id
+                          where l.IdAutor == id
+                          select new {
+                            id = l.Id,
+                            nombre = l.Nombre
+                          }).ToList();
+
+            ViewData["autor"] = nombreAutor;
+            ViewData["libros"] = libros;
             return View();
         }
     }
